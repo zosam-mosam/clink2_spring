90b6b1a37f0be272da5d39252d28cb04209d7a7f
zeegeon <zeegeon@naver.com>
2023-07-17 12:10:30 +0900

    merge

---
+++ clink/src/main/java/com/josam/clink/user/UserController.java
@@ -0,0 +1,48 @@
+package com.josam.clink.user;
+
+import java.net.http.HttpClient.Redirect;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	@Autowired
+	UserService userService;
+
+	@PostMapping("/join.do") // 얘는 되는 애,,
+	@ResponseBody
+	public String join(@RequestBody UserVO userVO) {
+//		System.out.println(userVO);
+		int r = userService.insert(userVO);
+		if (r == 0) {
+			return "no";
+		} else {
+			return "yes";
+		}
+	}
+	
+	@PostMapping("/login.do")
+	@ResponseBody
+	public String login(@RequestBody UserVO userVO) { 
+		System.out.println("도착했니?@!?!?!?!?!?!??!!!?");
+		int login = userService.login(userVO.getUserId(), userVO.getPwd());
+		System.out.println(login);
+		if(login == 0) {
+			return "fail";
+		}else {
+			// 토큰..? 저장
+			return "success";
+		}
+		
+	}
+}
--- clink/src/main/java/com/josam/clink/user/UserMapper.java
+++ clink/src/main/java/com/josam/clink/user/UserMapper.java
@@ -1,9 +1,11 @@
 package com.josam.clink.user;
 
+import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
 	
 	int insert(UserVO vo);
+	int login(String userId, String pwd);
 }
