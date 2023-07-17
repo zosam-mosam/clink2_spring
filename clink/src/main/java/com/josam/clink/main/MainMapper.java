da5c1a2cf48a6f411252df07741273119e08b5b5
zeegeon <zeegeon@naver.com>
2023-07-17 14:42:56 +0900

    Edit mybatis logic

--- clink/src/main/java/com/josam/clink/main/MainMapper.java
+++ clink/src/main/java/com/josam/clink/main/MainMapper.java
@@ -2,14 +2,14 @@ package com.josam.clink.main;
 
 import java.sql.Date;
 import java.util.List;
-import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface MainMapper {
 	
 	QuoteVO getQuote(int no);
-	List<MonthDataVO> getMonthData(String id);
-	DataVO getData(String id);
-	String getUserNo(String id);
+	List<MonthDataVO> getMonthData(@Param("userNo") int userNo, @Param("yesterday") Date yesterday);
+	DataVO getData(@Param("userNo") Integer id, @Param("yesterday") Date yesterday);
+	Integer getUserNo(String id);
 }
